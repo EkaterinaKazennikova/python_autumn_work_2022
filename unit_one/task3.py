30cd3ed270f2af4dc0da4fdf34e66831c20a1b01
Katerina <kazennikova.ekaterina@mail.ru>
2022-09-20 11:44:07 +0300

    Исправления

--- unit_one/task3.py
+++ unit_one/task3.py
@@ -2,7 +2,7 @@
 # Нужно обменять значения переменных местами. В итого age должен равнятся 25 а  temperature – 36.6:
 age = 36.6
 temperature = 25
-temperature = float(25)
-age, temperature = temperature, age
-print(int(age))
+a = temperature
+temperature = age
+print(a)
 print(temperature)
