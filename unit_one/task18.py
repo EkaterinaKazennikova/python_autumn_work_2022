a41e1dc2d90a111da4a296441543f88ebeb766ec
Katerina <kazennikova.ekaterina@mail.ru>
2022-10-17 14:17:27 +0300

    HW недоделано

--- unit_one/task18.py
+++ unit_one/task18.py
@@ -8,13 +8,24 @@
 #........................................
 #Выводы комбинаций оформить в список кортежей.
 
+def combo(x, y):
+    res = {}
+    for i in x:
+        for j in y:
+            summ = i + j
+            if summ not in res:
+                res[summ] = [(i, j)]
+            else:
+                res[summ] = res.get(summ) + [(i, j)]
+    return res
 
-from itertools import combinations
-num = [1, 2, 3, 4, 5, 6]
-dice1 = combinations(num, 2)
-dice2 = combinations(num, 2)
-for i in list(dice1):
-    for j in list(dice2):
-	    print ('сумма', sum(i + j), 'комбинация пар: кость 1',[i], 'кость2', [j])
+
+dice_1 = [1, 2, 3, 4, 5, 6]
+dice_2 = [1, 2, 3, 4, 5, 6]
+
+for key, var in combo(dice_1, dice_2).items():
+    print("Сумма", key, "Комбинация", var)
+
+
 
 
---
+++ unit_one/task31.py
@@ -0,0 +1,40 @@
+# todo:Реализовать декоратор в котором нужно подсчитать кол-во вызовов декорированной функции в процессе выполнения кода.
+# Выгрузить статистику подсчета в файл debug.log в формате: Название функции, кол-во вызовов, дата-время последнего выполнения
+# Пример:
+# render, 10,  12.05.2022 12:00
+# show,    5,  12.05.2022 12:02
+# render, 15,  12.05.2022 12:07
+
+# Декоратор должен применяться для различных функций с переменным числом аргументов.
+# Статистику вызовов необходимо записывать в файл при каждом запуске скрипта.
+import datetime
+import time
+#недоделано
+
+def decor_stat(func):
+    def wrapper():
+        count_calls = 0
+        start_time = time.perf_counter_ns()
+        end_time = start_time + time.perf_counter_ns()
+        result = func()
+        print(datetime.datetime.now(), end_time)
+        return result
+
+         #if i in range():
+         #  count_calls += 1
+         #retern counnt_calls
+
+    return wrapper()
+
+
+
+def stat():
+    render = 0
+    for i in range(1000):
+        render += 1
+
+    return render
+wp = decor_stat(stat)
+print(wp())
+
+
