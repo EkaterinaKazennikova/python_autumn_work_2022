71d085f77bcb725d22ad0735b04545d2a8305eb1
Katerina <kazennikova.ekaterina@mail.ru>
2022-11-25 14:12:53 +0300

    hw

--- unit_two/task35.py
+++ unit_two/task35.py
@@ -12,8 +12,8 @@ import json
 app = Flask(__name__)
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
-                            database='postgres',
-                            user='task',
+                            database='task',
+                            user='postgres',
                             password='4511')
     return conn
 
@@ -27,30 +27,32 @@ def get(self, id):
     return name
 
  #""" Передаем изменения в json """
+#router /student/{id}
 # def post(self, id):
-#     return jsonify(id)
+#      return jsonify(id)
+
+ #""" Добавить студента в json """
+# @app.route( /student/
+#     def put(self, id):
 
 
-    # router /student/
-    #def put(self, id):
-        #""" Добавить студента в json """
-
+# """ Удалим студента по {id}"""
     # router /student/{id}
     #def delete(self):
-        #""" Удалим студента по {id}"""
+
 
 
 # 5. Реализовать роутер /student/list - получение списка всех студентов
 # студенты могут храниться как в коллекции так и в БД
 
-# @app.route('/student/list')
-# def get(self):
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#     cur.execute('SELECT name FROM students_task')
-#     name = cur.fetchall()
-#     for row in name:
-#        return name
+@app.route('/student/list')
+def get(self):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT name FROM students_task')
+    name = cur.fetchall()
+    for row in name:
+       return name
 
 if __name__ == "__main__":
     app.run()
